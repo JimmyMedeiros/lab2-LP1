88a05e2de18579eca490bce731494f7879d19d9b
JoMedeiros <josivan.medeiros@yahoo.com.br>
2017-09-03 12:14:05 -0300

    Iniciando o main

--- src/questao01/main1.cpp
+++ src/questao01/main1.cpp
@@ -7,22 +7,64 @@
 #include "../../include/questao01/company.h"
 
 int main(int argc, char const *argv[]){
-	
-	Employee *empregado = new Employee();
+	char opt = -1;
+	std::vector<Company*> empresas;
+	std::cout << "Criando empresa\n";
+				/** TODO **/
 	Company *empresa = new Company();
-
-	empregado->wage = 1000.50;
-	std::cout << empregado->wage << std::endl;
 	empresa->CNPJ = 5050;
-	empresa->employees.push_back(*(&empregado));
-	std::cout << empresa->CNPJ << std::endl;
-	
-	std::vector<Employee*>::iterator it = empresa->employees.begin();
+	empresas.push_back(empresa);
+	std::vector<Company*>::iterator it = empresas.begin();
 
-	Employee emp = *(*it);
+	while (opt != 0){
+		std::cout << "\nEscolha uma das opções abaixo:\n"
+		<< "(1) Criar empresa\n"
+		<< "(2) Adicionar Funcionário\n"
+		<< "(0) Sair\n"
+		<< "Digite a sua opção: \n";
+		std::cin >> opt;
+		
+		if (not isdigit(opt) or (opt > '2')){
+			std::cout << "\nOpção inválida. Por favor, digite uma opção válida\n\n";
+		} 
+		else {
+			opt = atoi((const char*) &opt);
+		}
+
+		switch (opt){
+			case 1:
+			{
+				std::cout << "Criando empresa\n";
+				/** TODO **/
+				Company *empresa = new Company();
+				empresa->CNPJ = 5050;
+				empresas.push_back(empresa);
+			}
+				break;
+			case 2:
+			{
+				std::cout << "Escolha uma empresa para cadastrar o funcionário\n";
+				/** TODO : Verify if the vector of employees was inicialized **/
+				Employee *empregado = new Employee();
+				empregado->wage = 937.00;
+				std::vector<Company*>::iterator it = empresas.begin();
+				(*it)->employees.push_back(empregado);
+			}
+				break;
+			default:
+				break;
+		}
+	}
+
+
+	/**std::cout << it->CNPJ << std::endl;
+	
+	std::vector<Employee*>::iterator it2 = it->employees.begin();
+
+	Employee emp = *(*it2);
 	float sal = emp.wage;
 
-	std::cout << sal << std::endl;
+	std::cout << sal << std::endl;**/
 
 	return 0;
 }
